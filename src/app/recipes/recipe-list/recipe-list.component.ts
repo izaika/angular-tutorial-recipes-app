a3a209f652a4556e5e3bd1e505d54f398ca4ba8d
Igor Zaika <izaika@Igors-MacBook-Pro.local>
2017-05-06 13:16:17 +0300

    recipes layout in progress

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test Recipe',
+      'This is a simply test',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/KFC_Original_Recipe_chicken_in_bucket.jpg/1280px-KFC_Original_Recipe_chicken_in_bucket.jpg'
+    ),
+    new Recipe(
+      'Test Recipe',
+      'This is a simply test',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/KFC_Original_Recipe_chicken_in_bucket.jpg/1280px-KFC_Original_Recipe_chicken_in_bucket.jpg'
+    ),
+  ];
 
   constructor() { }
 
