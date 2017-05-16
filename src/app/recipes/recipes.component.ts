9fee1162248e6cd21a5f11aa452bdca95eb1fb14
Igor Zaika <izaika89@gmail.com>
2017-05-16 17:33:51 +0300

    Add shopping list service

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -15,11 +15,9 @@ export class RecipesComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.recipeService.recipeSelected.subscribe(
-      (recipe: Recipe) => {
+    this.recipeService.recipeSelected.subscribe((recipe: Recipe) => {
         this.selectedRecipe = recipe;
-      }
-    );
+     });
   }
 
 }
