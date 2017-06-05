efde45adda72e48c70829064203466236ccde03d
Igor Zaika <izaika@Igors-MacBook-Pro.local>
2017-06-05 10:56:42 +0300

    create signup/signin components, set up firebase sdk for authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   loadedFeature = 'recipe';
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyD4NODjY4dMyB34z0qAHriYqddoj_3MxME',
+      authDomain: 'udemy-ng-http-daa1b.firebaseapp.com'
+    });
+  }
 
   onNavigate(feature: string) {
     this.loadedFeature = feature;
   }
+
 }
