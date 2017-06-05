3ab4a62bf6791fa72c73819a298c42f7e6e344dd
Igor Zaika <izaika@Igors-MacBook-Pro.local>
2017-06-05 12:03:13 +0300

    add auth token to protected requests to firebase

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,11 +1,15 @@
 import * as firebase from 'firebase';
 
 export class AuthService {
+  token: string;
+
   signupUser(email: string, password: string) {
     firebase
       .auth()
       .createUserWithEmailAndPassword(email, password)
-      .then(response => console.log(response))
+      .then((response) => {
+        firebase.auth().currentUser.getToken().then((token: string) => this.token = token);
+      })
       .catch(error => console.log(error));
   }
 
@@ -15,5 +19,10 @@ export class AuthService {
       .signInWithEmailAndPassword(email, password)
       .then(response => console.log(response))
       .catch(error => console.log(error));
+  }
+
+  getToken() {
+    firebase.auth().currentUser.getToken().then((token: string) => this.token = token);
+    return this.token;
   }
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -5,21 +5,27 @@ import {RecipeService} from '../recipes/recipe.service';
 import 'rxjs/Rx';
 import {Observable} from 'rxjs/Observable';
 import {Recipe} from '../recipes/recipe.model';
+import {AuthService} from '../auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
   private dbURL = 'https://udemy-ng-http-daa1b.firebaseio.com/';
 
   constructor(private http: Http,
-              private recipeService: RecipeService) {
+              private recipeService: RecipeService,
+              private authService: AuthService) {
   }
 
   storeRecipes(): Observable<Response> {
-    return this.http.put(`${this.dbURL}recipes.json`, this.recipeService.getRecipes());
+    const token = this.authService.getToken();
+
+    return this.http.put(`${this.dbURL}recipes.json?auth=${token}`, this.recipeService.getRecipes());
   }
 
   getRecipes() {
-    return this.http.get(`${this.dbURL}recipes.json`)
+    const token = this.authService.getToken();
+
+    return this.http.get(`${this.dbURL}recipes.json?auth=${token}`)
       .map((response: Response) => {
         const recipes: Recipe[] = response.json();
         for (const recipe of recipes) {
@@ -30,8 +36,8 @@ export class DataStorageService {
         return recipes;
       })
       .subscribe((recipes: Recipe[]) => {
-      this.recipeService.setRecipes(recipes);
-    });
+        this.recipeService.setRecipes(recipes);
+      });
   }
 
 }
